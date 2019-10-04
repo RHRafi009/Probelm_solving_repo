926162f7a65746644bd1ad40ff51c6d0161ca549
RHRafi900 <kh.rashed900@gmail.com>
2019-10-04 22:14:05 +0600

    Solution of Arrays-DS probelm.  The solution is inside of the Arrays-DS.cs class.

---
+++ Data_Structure_HackerRank/Arrays/Arrays-DS.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structure_HackerRank.Arrays
+{
+    class Arrays_DS
+    {
+        //the reverseArray function
+        public static int[] reverseArray(int[] a)
+        {
+            int[] arr = new int[a.Length];
+            int i = 0, j = a.Length - 1;
+            for (; i < a.Length; i++, j--)
+                arr[i] = a[j];
+            return arr;
+        }
+    }
+}
--- Data_Structure_HackerRank/Program.cs
+++ Data_Structure_HackerRank/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Data_Structure_HackerRank.Arrays;
 
 namespace Data_Structure_HackerRank
 {
@@ -10,6 +11,13 @@ namespace Data_Structure_HackerRank
     {
         static void Main(string[] args)
         {
+            int[] arr = Arrays_DS.reverseArray(new int[] { 2,3,4,5});
+            foreach (int i in arr)
+                Console.Write(i + " ");
+
+
+            //to make output console visible
+            Console.ReadKey();  
         }
     }
 }
