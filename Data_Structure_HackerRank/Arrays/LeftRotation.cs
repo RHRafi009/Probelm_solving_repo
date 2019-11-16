5189b9d14295b1e3ec414f7297762220f9161fe5
RHRafi900 <kh.rashed900@gmail.com>
2019-11-16 17:26:18 +0600

    LeftRotation URl : https://www.hackerrank.com/challenges/array-left-rotation/problem

---
+++ Data_Structure_HackerRank/Arrays/LeftRotation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structure_HackerRank.Arrays
+{
+    class LeftRotation
+    {
+
+        // This will fail test case 6 and 9 due to large number of values 
+
+        public static int[] LeftRotate(int[] arr, int d)
+        {
+            int[] temp = new int[d];
+            for(int i=0; i<d; i++)
+            {
+                temp[i] = arr[i];
+            }
+
+            for(int i=0, j=d; i<d && j<arr.Length; i++, j++)
+            {
+                arr[i] = arr[j];
+            }
+            for (int i = (arr.Length - d), j=0; i < arr.Length && j<temp.Length; i++, j++)
+            {
+                arr[i] = temp[j];
+            }
+            return arr;
+        }
+    }
+}
