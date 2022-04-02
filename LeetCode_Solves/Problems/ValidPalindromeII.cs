bd36da262ce96c8f13c8f646fee7cb960500077b
Khandakar Rashed Hassan <kh.rashed900@gmail.com>
2022-04-03 00:59:07 +0600

    Added leetcode project.

---
+++ LeetCode_Solves/Problems/ValidPalindromeII.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solves.Problems
+{
+    /*
+     * Problem link - https://leetcode.com/problems/valid-palindrome-ii/
+     */
+    public class ValidPalindromeII
+    {
+        public bool ValidPalindrome(string s)
+        {
+            return true;
+        }
+    }
+}
