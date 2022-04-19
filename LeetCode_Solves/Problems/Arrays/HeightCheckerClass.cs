df2732202ef12e2644d32cc5bd76b683fe3f4c7f
Khandakar Rashed Hassan <kh.rashed900@gmail.com>
2022-04-19 16:58:10 +0600

    Height checker problem solved.

---
+++ LeetCode_Solves/Problems/Arrays/HeightCheckerClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solves.Problems.Arrays
+{
+    /*
+     * Problem link - https://leetcode.com/explore/learn/card/fun-with-arrays/523/conclusion/3228/
+     */
+    public class HeightCheckerClass
+    {
+        public int HeightChecker(int[] heights)
+        {
+            var heightsList = heights.ToList();
+            heightsList.Sort();
+            int mismatchedIndices = 0;
+            for(int i = 0; i<heights.Length; i++)
+            {
+                if (heights[i] != heightsList[i])
+                    mismatchedIndices++;
+            }
+            return mismatchedIndices;
+        }
+    }
+}
